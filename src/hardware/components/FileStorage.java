f79f3617e8c2905924c7029ef6fd427ff4b8d42e
jkuznik <janusz.kuznik89@gmail.com>
2025-01-01 19:20:46 +0100

    Add methods in FileStorage

--- src/hardware/components/FileStorage.java
+++ src/hardware/components/FileStorage.java
@@ -3,14 +3,31 @@ package hardware.components;
 import software.file.File;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class FileStorage {
 
-    final int capacity;
+    int capacity;
 
     List<File> files;
 
     public FileStorage(int capacity) {
         this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void addFile(File file) {
+        files.add(file);
+        capacity -= file.getSize();
+    }
+
+    public File getFile(File file) {
+        return files.stream()
+                .filter(f -> f.equals(file))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("File not found"));
     }
 }
