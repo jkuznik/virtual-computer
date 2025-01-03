fa2740b8e5d8b69ea00c580a90011f469ed46e13
jkuznik <janusz.kuznik89@gmail.com>
2025-01-03 16:42:03 +0100

    Add GameHandler, update Game interface

--- src/software/game/Game.java
+++ src/software/game/Game.java
@@ -1,6 +1,7 @@
 package software.game;
 
 public interface Game {
+    void getName(String name);
     void startGame();
     void endGame();
 }
---
+++ src/software/game/GameHandler.java
@@ -0,0 +1,17 @@
+package software.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHandler {
+
+    private final List<Game> games = new ArrayList<>();
+
+    public List<Game> gameList() {
+        return games;
+    }
+
+    public void startGameByName(String name) {
+
+    }
+}
