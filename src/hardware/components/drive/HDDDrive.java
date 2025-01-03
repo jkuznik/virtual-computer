ba44ec89a869c471816773924a6453712c019514
jkuznik <janusz.kuznik89@gmail.com>
2025-01-03 16:03:38 +0100

    Extend AbstractDrive by HDDDrive and SDDDrive

--- src/hardware/components/drive/HDDDrive.java
+++ src/hardware/components/drive/HDDDrive.java
@@ -1,57 +1,10 @@
 package hardware.components.drive;
 
-import hardware.components.shared.enums.ComponentType;
-import hardware.components.shared.FileHandler;
-import hardware.components.shared.FileStorage;
 import hardware.components.shared.enums.StorageCapacity;
-import software.file.File;
 
-public class HDDDrive implements Drive, FileHandler {
-    private final FileStorage fileStorage;
-    private final String name;
+public class HDDDrive extends AbstractDrive{
 
     public HDDDrive(StorageCapacity storageCapacity, String name) {
-        this.fileStorage = new FileStorage(storageCapacity);
-        this.name = name;
-    }
-//  TODO: metoda do zaimplementowania w kolejnym tasku
-    @Override
-    public void getWriteSpeed() {
-
-    }
-    //  TODO: metoda do zaimplementowania w kolejnym tasku
-    @Override
-    public void getReadSpeed() {
-
-    }
-
-    @Override
-    public void addFile(File file) {
-        fileStorage.addFile(file);
-    }
-
-    @Override
-    public void listFiles() {
-        fileStorage.listFiles();
-    }
-
-    @Override
-    public void removeFile(File file) {
-        fileStorage.removeFile(file);
-    }
-
-    @Override
-    public File findFile(String fileName) {
-        return fileStorage.findFile(fileName);
-    }
-
-    @Override
-    public String getComponentName() {
-        return name;
-    }
-
-    @Override
-    public ComponentType getComponentType() {
-        return ComponentType.DRIVE;
+        super(storageCapacity, name);
     }
 }
--- src/hardware/components/drive/SSDDrive.java
+++ src/hardware/components/drive/SSDDrive.java
@@ -1,59 +1,10 @@
 package hardware.components.drive;
 
-import hardware.components.shared.enums.ComponentType;
-import hardware.components.shared.FileHandler;
-import hardware.components.shared.FileStorage;
 import hardware.components.shared.enums.StorageCapacity;
-import software.file.File;
 
-public class SSDDrive implements Drive, FileHandler {
-    private final FileStorage fileStorage;
-    private final String name;
+public class SSDDrive extends AbstractDrive{
 
     public SSDDrive(StorageCapacity storageCapacity, String name) {
-        this.fileStorage = new FileStorage(storageCapacity);
-        this.name = name;
-    }
-
-    //  TODO: metoda do zaimplementowania w kolejnym tasku
-    @Override
-    public void getWriteSpeed() {
-
-    }
-
-    //  TODO: metoda do zaimplementowania w kolejnym tasku
-    @Override
-    public void getReadSpeed() {
-
-    }
-
-    @Override
-    public void addFile(File file) {
-        fileStorage.addFile(file);
-    }
-
-    @Override
-    public void listFiles() {
-        fileStorage.listFiles();
-    }
-
-    @Override
-    public void removeFile(File file) {
-        fileStorage.removeFile(file);
-    }
-
-    @Override
-    public File findFile(String fileName) {
-        return fileStorage.findFile(fileName);
-    }
-
-    @Override
-    public String getComponentName() {
-        return name;
-    }
-
-    @Override
-    public ComponentType getComponentType() {
-        return ComponentType.DRIVE;
+        super(storageCapacity, name);
     }
 }
