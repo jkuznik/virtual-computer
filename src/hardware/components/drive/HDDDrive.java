e82e04eca78b3187264f5bc40b1310d48a1ea045
jkuznik <janusz.kuznik89@gmail.com>
2025-01-03 16:06:12 +0100

    Add TODO's

--- src/hardware/components/drive/HDDDrive.java
+++ src/hardware/components/drive/HDDDrive.java
@@ -4,6 +4,8 @@ import hardware.components.shared.enums.StorageCapacity;
 
 public class HDDDrive extends AbstractDrive{
 
+    //TODO: dodać pola w klasie HDDDrive oraz SDDDrive rozróżniające te dwie klasy lub dodać nowe typy w ComponentType
+    // dedykowane dla HDD oraz SDD i nadpisać metody getComponentType() w tych klasach
     public HDDDrive(StorageCapacity storageCapacity, String name) {
         super(storageCapacity, name);
     }
