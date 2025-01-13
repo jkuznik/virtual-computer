1a9d80137f9dc82d31b4ef892b05d454c9250aac
jkuznik <janusz.kuznik89@gmail.com>
2025-01-13 13:04:47 +0100

    Add ComponentNotFoundException

---
+++ src/hardware/components/shared/ComponentNotFoundException.java
@@ -0,0 +1,7 @@
+package hardware.components.shared;
+
+public class ComponentNotFoundException extends Exception {
+    public ComponentNotFoundException(String message) {
+        super(message);
+    }
+}
