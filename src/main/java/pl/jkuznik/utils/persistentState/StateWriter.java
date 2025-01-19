6484a2495a19d8f26a7fc9aca36e30974ee7c508
jkuznik <janusz.kuznik89@gmail.com>
2025-01-19 15:23:01 +0100

    Add toJson() method in Component interface

--- src/main/java/pl/jkuznik/utils/persistentState/StateWriter.java
+++ src/main/java/pl/jkuznik/utils/persistentState/StateWriter.java
@@ -1,4 +1,20 @@
 package pl.jkuznik.utils.persistentState;
 
+import com.google.gson.Gson;
+import pl.jkuznik.computer.hardware.shared.Component;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class StateWriter {
+
+    private final Path path = Paths.get(FilePath.COMPUTER_STATE.getPath());
+    private final Gson gson = new Gson();
+
+    public void writeState(List<Component> components) {
+        components.forEach(component -> {
+
+        });
+    }
 }
