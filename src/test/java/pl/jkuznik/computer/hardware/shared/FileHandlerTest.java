58118d5a7a648ae3536ec0283b61c3733b96b18f
jkuznik <janusz.kuznik89@gmail.com>
2025-02-04 18:45:02 +0100

    Test FileHandler methods

--- src/test/java/pl/jkuznik/computer/hardware/shared/FileHandlerTest.java
+++ src/test/java/pl/jkuznik/computer/hardware/shared/FileHandlerTest.java
@@ -3,7 +3,11 @@ package pl.jkuznik.computer.hardware.shared;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.jkuznik.computer.hardware.shared._enums.StorageCapacity;
+import pl.jkuznik.computer.software.file.File;
 import pl.jkuznik.computer.software.file.imagefile.JPGImageFile;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,6 +38,7 @@ class FileHandlerTest {
     void shouldRemoveFile_whenFileExists() {
         given();
         var file = new JPGImageFile("foo", 5,5);
+        fileHandler.addFile(file);
         int before = fileHandler.getFiles().size();
 
         when();
@@ -46,19 +51,67 @@ class FileHandlerTest {
     }
 
     @Test
-    void findFile() {
+    void shouldReturnFile_whenFileExists() throws FileNotFoundException {
+        given();
+        var file = new JPGImageFile("foo", 5,5);
+        fileHandler.addFile(file);
+
+        when();
+        File result = fileHandler.findFile("foo");
+
+        then();
+        assertNotNull(result);
+        assertEquals(file, result);
+        assertEquals("foo", result.getName());
     }
 
     @Test
-    void getCurrentCapacity() {
+    void shouldThrowException_whenFileDoesNotExist() {
+        given();
+
+        when();
+
+        then();
+        assertThrows(FileNotFoundException.class, () -> fileHandler.findFile("bar"));
     }
 
     @Test
-    void getStorageCapacity() {
+    void shouldReturnCurrentCapacity() {
+        given();
+
+        when();
+
+        then();
+        assertEquals(0, fileHandler.getCurrentCapacity());
+    }
+
+    @Test
+    void shouldReturnStorageCapacity() {
+        given();
+
+        when();
+
+        then();
+        assertEquals(StorageCapacity.GB1, fileHandler.getStorageCapacity());
     }
 
     @Test
     void getFiles() {
+        given();
+        var file1 = new JPGImageFile("foo", 5,5);
+        var file2 = new JPGImageFile("bar", 5,5);
+        var file3 = new JPGImageFile("baz", 5,5);
+        fileHandler.addFile(file1);
+        fileHandler.addFile(file2);
+
+        when();
+        List<File> files = fileHandler.getFiles();
+
+        then();
+        assertEquals(2, files.size());
+        assertTrue(files.contains(file1));
+        assertTrue(files.contains(file2));
+        assertFalse(files.contains(file3));
     }
 
     private void given() {
