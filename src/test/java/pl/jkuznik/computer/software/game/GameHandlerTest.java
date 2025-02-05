95615e5177cc101590d359c01e7b5b006b22239a
jkuznik <janusz.kuznik89@gmail.com>
2025-02-05 20:12:49 +0100

    Increase test coverage using reflection

--- src/test/java/pl/jkuznik/computer/software/game/GameHandlerTest.java
+++ src/test/java/pl/jkuznik/computer/software/game/GameHandlerTest.java
@@ -2,7 +2,9 @@ package pl.jkuznik.computer.software.game;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +13,7 @@ class GameHandlerTest {
     private GameHandler gameHandler = GameHandler.getInstance();
 
     @Test
-    void gameList() {
+    void shouldReturnNotEmptyGameList() {
         given();
         
         when();
@@ -22,7 +24,21 @@ class GameHandlerTest {
     }
 
     @Test
-    void startGameByName() {
+    void shouldStartGame_whenGameExists() throws NoSuchFieldException, IllegalAccessException, GameNotFoundException {
+        given();
+        Field gamesField = gameHandler.getClass().getDeclaredField("games");
+        gamesField.setAccessible(true);
+        var gameList = (List<Game>) gamesField.get(gameHandler);
+
+        Game game = Mockito.mock(Game.class);
+        gameList.add(game);
+
+        when();
+        Mockito.when(game.getName()).thenReturn("foo");
+
+        then();
+        gameHandler.startGameByName("foo");
+        Mockito.verify(game).startGame();
     }
 
     private void given() {
