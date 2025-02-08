f0d4122505a75b1db3874f8de153fdbc1b0f7ec3
jkuznik <janusz.kuznik89@gmail.com>
2025-02-08 15:42:48 +0100

    Fix serialization issue caused by FileType return type refactor

--- src/main/java/pl/jkuznik/utils/persistentState/StateWriter.java
+++ src/main/java/pl/jkuznik/utils/persistentState/StateWriter.java
@@ -3,8 +3,12 @@ package pl.jkuznik.utils.persistentState;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import pl.jkuznik.computer.hardware.shared.Component;
+import pl.jkuznik.computer.hardware.shared.FileHandler;
+import pl.jkuznik.computer.software.file.File;
 import pl.jkuznik.utils._enums.FilePath;
 import pl.jkuznik.utils.persistentState.gsonTypeAdapter.ComponentGsonAdapter;
+import pl.jkuznik.utils.persistentState.gsonTypeAdapter.FileAdapterGson;
+import pl.jkuznik.utils.persistentState.gsonTypeAdapter.FileHandlerAdapterGson;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,6 +22,8 @@ public class StateWriter {
 
     private transient final Gson gson = new GsonBuilder()
             .registerTypeHierarchyAdapter(Component.class, new ComponentGsonAdapter())
+            .registerTypeHierarchyAdapter(FileHandler.class, new FileHandlerAdapterGson())
+            .registerTypeHierarchyAdapter(File.class, new FileAdapterGson())
             .create();
 
     public void writeState(Set<Component> components) {
